57663467e675806b728aa2f162fb0e322df5c3bd
Ricardo Castrellon <ricardocastrellon10@gmail.com>
2022-04-14 12:08:59 -0700

    basic death screen overlay

---
+++ Assets/GameOverScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameOverScript : MonoBehaviour
+{
+    public void Setup(int Score)
+    {
+        gameObject.SetActive(true);
+    }
+}
--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -37,6 +37,7 @@ public class PauseMenu : MonoBehaviour
     
     public void LoadMenu()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("Main Menu");
     }
 
