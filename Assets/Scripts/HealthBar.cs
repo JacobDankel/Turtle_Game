cb7aaf8b9630534e3b275abb4b8e355270489ff6
oscarmorales2016 <90660313+oscarmorales2016@users.noreply.github.com>
2022-07-30 11:51:07 -0700

    push

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Slider slider;
+    public void SetMaxHealth(float health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+    public void SetHealth(float health)
+    {
+        slider.value = health;
+    }
+
+   
+}
